c6d84d2988e8233f4366135890179eb2fb0527ce
Thomas Scott <fcbtomtom6@gmail.com>
2024-06-12 23:31:50 -0700

    got preview cards down, search results now refresh when making a new search, search results now store json for data, still need to add select functon and the entire search mechanism for ingredients

--- public/js/index.js
+++ public/js/index.js
@@ -8,15 +8,21 @@ let drinkTypeInput = document.getElementById('drink-type-input');
 let cupTypeSwitch = document.getElementById('cup-type-switch');
 let cupTypeInput = document.getElementById('cup-type-input');
 let searchResults = document.getElementById('search-results');
+
+let previewName = document.getElementById('preview-name');
+let previewImage = document.getElementById('preview-image');
+let previewInstructions = document.getElementById('instructions-list');
 const url = "http://localhost:3000";
 
 searchInput.addEventListener('keypress', e => {
     if(e.key === 'Enter') {
+        removeSearchResults();
         search();
     }
 })
 
 searchButton.addEventListener('click', () => {
+    removeSearchResults();
     search();
 })
 
@@ -43,6 +49,7 @@ async function search() {
         for(drink of data) {
             let mainDiv = document.createElement('div');
             mainDiv.className = 'search-result-wrapper';
+            mainDiv.setAttribute('data-drink', JSON.stringify(drink));
             let resultThumb = document.createElement('img');
             resultThumb.src = drink.thumbnail + '/preview';
             resultThumb.className = 'search-result-thumbnail';
@@ -64,11 +71,41 @@ async function search() {
             mainDiv.appendChild(resultCupType);
             mainDiv.appendChild(resultAlcoholic);
             searchResults.appendChild(mainDiv);
+            mainDiv.addEventListener('mouseover', () => {
+                //console.log(mainDiv.getAttribute('data-drink'));
+                updatePreview(JSON.parse(mainDiv.getAttribute('data-drink')));
+            })
         }
         
         
     } else {
         //////////
+    }
+}
+
+function updatePreview(drink) {
+    document.getElementById('instructions-title').style.visibility = 'visible';
+    previewName.innerHTML = drink.name;
+    previewImage.src = drink.thumbnail + '/preview';
+    removeChildElements(previewInstructions);
+    for(let i = 0; i < drink.measurements.length; i++) {
+        let instruction = document.createElement('li');
+        instruction.className = 'preview-instruction';
+        instruction.innerHTML = (drink.measurements[i] !== null ? drink.measurements[i].charAt(0).toUpperCase() + drink.measurements[i].slice(1) + (drink.measurements[i].charAt(drink.measurements[i].length - 1) === ' ' ? '' : ' ') : "") + drink.ingredients[i];
+        previewInstructions.appendChild(instruction);
+    }
+}
+
+function removeChildElements(node) {
+    for(let i = node.childNodes.length - 1; i >= 0; i--) {
+        node.removeChild(node.childNodes[i]);
+    }
+}
+
+function removeSearchResults() {
+    let currentResults = document.getElementsByClassName('search-result-wrapper');
+    for(let i = currentResults.length - 1; i >= 0; i--) {
+        currentResults[i].remove();
     }
 }
 
